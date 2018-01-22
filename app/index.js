86c96694db718a188b11da5e687050cfd045bfdf
aaronRhodebeck <arhodebeck@gmail.com>
2018-01-22 17:24:01 -0600

    Initial setup complete

---
+++ app/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main.jsx";
+
+const App = () => <Main />;
+ReactDOM.render(<App />, document.getElementById("app"));
