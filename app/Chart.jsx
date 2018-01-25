f19d84bfdfb3efe09fab6597af3a6368dd16553b
aaronRhodebeck <arhodebeck@gmail.com>
2018-01-25 12:11:06 -0600

    Chart is finished

--- app/Chart.jsx
+++ app/Chart.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 import { withFauxDOM } from 'react-faux-dom';
+import makeEducationMap from './makeEducationMap';
 
 const ChartContainer = styled.div`
   margin: auto;
-  height: 80vh;
+  height: 100%;
   box-shadow: 1px 1px 6px rgba(200, 200, 200, 0.4), -1px -1px 6px rgba(200, 200, 200, 0.3);
+  padding: 5px;
 `;
 
-export default class Chart extends React.Component {
+class Chart extends React.Component {
   componentWillMount() {
-    console.log(this.props.educationData, this.props.topographicData);
+    const faux = this.props.connectFauxDOM('div', 'chart');
+    makeEducationMap(faux, this.props.educationData, this.props.topographicData, this);
   }
   render() {
-    return <ChartContainer>Chart is in progress</ChartContainer>;
+    return <ChartContainer>{this.props.chart || 'Chart is in progress'}</ChartContainer>;
   }
 }
+export default withFauxDOM(Chart);
